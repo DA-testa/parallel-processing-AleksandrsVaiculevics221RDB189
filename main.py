39cb6f9df331a4d6a07687178b827a26fffed182
AleksandrsVaiculevics221RDB189 <124890233+AleksandrsVaiculevics221RDB189@users.noreply.github.com>
2023-03-18 22:14:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,44 @@
 # python3
-
-def parallel_processing(n, m, data):
-    output = []
+import heapq
     # TODO: write the function for simulating parallel tasks, 
     # create the output pairs
-
-    return output
-
-def main():
     # TODO: create input from keyboard
     # input consists of two lines
     # first line - n and m
     # n - thread count 
     # m - job count
-    n = 0
-    m = 0
-
     # second line - data 
     # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
-    data = []
-
     # TODO: create the function
-    result = parallel_processing(n,m,data)
-    
     # TODO: print out the results, each pair in it's own line
+def parallel_processing(n, k, data):
+    output = []
+    t = [(0, i) for i in range(n)]
+    heapq.heapify(t)
+    for i in range(k):
+       start, ind = heapq.heappop(t)
+        output.append((ind, start))
+        finish = start + data[i]
+        heapq.heappush(t, (finish, ind))
+    return output
 
-
+def main():
+    try:
+        n, k = map(int, input().split())
+        if not (1 <=n <= 10**5) or not (1 <= k <= 10**5):
+            raise ValueError("Invalid input, input must be from 1 to 10^5")
+        
+        data = list(map(int, input(). split()))
+        if len(data) != k or not all(1 <= d <= 10**9 for d in data):
+            raise ValueError("Invalid input: data must be a list of m integers between 1 and 10^9")
+            
+        result = parallel_processing(n, k, data)
+        
+        for i in range(k):
+            print(result[i][0], result[i][1])
+            
+    except ValueError as e:
+        print (e)
 
 if __name__ == "__main__":
     main()
